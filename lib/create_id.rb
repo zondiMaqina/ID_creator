24f91a50841a966a64f9deb464c795c589ccfd81
zondimaqina <zondimaqina0@gmail.com>
2024-06-09 16:03:18 +0200

    add instant method and comment

--- lib/create_id.rb
+++ lib/create_id.rb
@@ -1,6 +1,17 @@
+# class implementation
 class Details
-  attr_reader :name, :surname, :sex, :nationality, :race, :status, :id_number
-  def initialize
-    
+  attr_reader :names, :surname, :sex, :nationality, :race, :status, :id_number
+
+  def initialize(names, surname, sex, nationality)
+    @names = names.split(' ')
+    @surname = surname
+    @sex = sex
+    @nationality = nationality
+  end
+
+  def personal_info(race, status, id_number)
+    @race = race
+    @status = status
+    @id_number = id_number
   end
 end
