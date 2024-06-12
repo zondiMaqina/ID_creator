74d8eb59499641a22fb625e1a2126e877d46c5c9
zondimaqina <zondimaqina0@gmail.com>
2024-06-12 19:49:16 +0200

    add gem features

--- lib/receive_info.rb
+++ lib/receive_info.rb
@@ -3,70 +3,70 @@ require_relative 'create_id'
 class RecieveInfo
   def initialize
     super
-    puts 'WELCOME USER!!! LET US CREATE YOUR INFORMATION IDENTITY (I.D), Enter some details below'
+    puts 'WELCOME USER!!! LET US CREATE YOUR INFORMATION IDENTITY (I.D), Enter some details below'.colorize(:light_yellow)
     basic_info
   end
 
   private
 
   def names
-    puts 'Please enter your name(s)'
+    puts 'Please enter your name(s)'.colorize(:green)
     gets.chomp
   end
 
   def surname
-    puts "\nPlease enter your surname"
+    puts "\nPlease enter your surname".colorize(:green)
     gets.chomp
   end
 
   def sex
-    puts "\nplease enter your gender male/female"
+    puts "\nplease enter your gender male/female".colorize(:green)
     sex = gets.chomp
     until sex.include?('male') || sex.include?('female')
-      puts "\nplease enter male/female"
+      puts "\nplease enter male/female".colorize(:green)
       sex = gets.chomp
     end
     sex
   end
 
   def nationality
-    puts "\nPlease eneter your nationality"
+    puts "\nPlease eneter your nationality".colorize(:green)
     gets.chomp
   end
 
   def status
-    puts 'Please enter your relationship status'
+    puts 'Please enter your relationship status'.colorize(:green)
     status = gets.chomp.downcase
     until status.include?('single') || status.include?('married')
-      puts "\nPlease enter single/married"
+      puts "\nPlease enter single/married".colorize(:green)
       status = gets.chomp.downcase
     end
     status
   end
 
   def id_number
-    puts "\nPlease enter your ID number"
+    puts "\nPlease enter your ID number".colorize(:green)
     id_number = gets.chomp.gsub(' ', '')
     until id_number.length == 13
-      puts "\nPlease enter a valid ID number"
+      puts "\nPlease enter a valid ID number".colorize(:green)
       id_number = gets.chomp.downcase
     end
     id_number
   end
 
   def race
-    puts 'Please enter your race'
+    puts 'Please enter your race'.colorize(:green)
     race = gets.chomp
 
     until race.downcase == 'black' || race.downcase == 'white'
-      puts 'Please enter your race => black/white'
+      puts 'Please enter your race => black/white'.colorize(:green)
       race = gets.chomp
     end
     race
   end
 
   def individual_info
-    puts "\nPERSONAL DETAILS"
+    puts "\nPERSONAL DETAILS".colorize(:red)
     race
   end
 
